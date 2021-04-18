9b0122d792009802ad0d05029915fa9c59eff995
=pauladerecho <=pauladerechodam@gmail.com>
2021-04-18 21:19:27 +0200

    Pantalla login creada

--- app/src/main/java/com/example/proyectoedia/MainActivity.java
+++ app/src/main/java/com/example/proyectoedia/MainActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }
