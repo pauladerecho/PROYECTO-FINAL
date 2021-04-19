752dff0b3004c6298a531fb29b955b83faef7a31
=pauladerecho <=pauladerechodam@gmail.com>
2021-04-19 13:36:31 +0200

    prueba paula

--- app/src/main/java/com/example/proyectoedia/MainActivity.java
+++ app/src/main/java/com/example/proyectoedia/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("tag","Hola soy sara");
 
-        Log.v("tag", "Soy paula");
+        Log.v("prueba", "Soy paula");
 
     }
 }
