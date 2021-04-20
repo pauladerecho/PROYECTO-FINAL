73a9cbe15e028ce85a6a9257f3afca7e40cc8ebc
saraardila <saraardiladam@gmail.com>
2021-04-20 16:51:15 +0200

    otro cambio

--- app/src/main/java/com/example/proyectoedia/MainActivity.java
+++ app/src/main/java/com/example/proyectoedia/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.v("tag","Hola soy sara");
+        Log.v("tag","hola soy paula");
     }
 }
