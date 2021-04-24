99acb8a572a4d2c160ac029cd53a9401ad229cfd
=pauladerecho <=pauladerechodam@gmail.com>
2021-04-24 12:37:34 +0200

    Inicio con fallo

--- app/src/main/java/com/example/proyectoedia/MainActivity.java
+++ app/src/main/java/com/example/proyectoedia/MainActivity.java
@@ -2,15 +2,21 @@ package com.example.proyectoedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseAuth firebaseAuth;
+    FirebaseAuth mAuth;
     DatabaseReference databaseReference;
 
     @Override
@@ -18,9 +24,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("tag","Hola soy sara");
+        FirebaseUser user = mAuth.getCurrentUser();
 
-        Log.v("prueba", "Soy paula");
+        String email = user.getEmail();
+        String uid = user.getUid();
 
+        HashMap<Object, String> hashMap = new HashMap<>();
+        hashMap.put("email", email);
+        hashMap.put("uid", uid);
+        hashMap.put("telefono", "");
+        hashMap.put("imagen", "");
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference("Users");
+        reference.child(uid).setValue(hashMap);
+
+
+    }
+
+    public void botonEntrar(View v){
+
+        Intent i = new Intent(this, PantallaInicio_Activity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/proyectoedia/PantallaInicio_Activity.java
@@ -0,0 +1,15 @@
+package com.example.proyectoedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PantallaInicio_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_inicio_);
+
+
+    }
+}
