ae3e0affedfc2969f5dc89f5cb4e374226bcba0c
saraardila <saraardiladam@gmail.com>
2021-05-10 16:31:18 +0200

    error que no se que es

--- app/src/main/java/com/example/proyectoedia/menu/Chat/AdaptadorChat.java
+++ app/src/main/java/com/example/proyectoedia/menu/Chat/AdaptadorChat.java
@@ -1,13 +1,17 @@
 package com.example.proyectoedia.menu.Chat;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
@@ -16,9 +20,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.proyectoedia.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
@@ -57,7 +68,7 @@ public class AdaptadorChat extends RecyclerView.Adapter<AdaptadorChat.MyHolder> 
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
-    public void onBindViewHolder(@NonNull MyHolder holder, int i) {
+    public void onBindViewHolder(@NonNull MyHolder holder, final int i) {
         //-->>Recuperar los datos
 
         String mensaje = chatList.get(i).getMensaje();
@@ -81,6 +92,39 @@ public class AdaptadorChat extends RecyclerView.Adapter<AdaptadorChat.MyHolder> 
         }catch (Exception e){
         }
 
+       /* //-->> onClick dialog para el borrado de un mensaje
+        holder.mensajeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //-->> Alerta para confirmar el borrado
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Borrado");
+                builder.setMessage("¿Estás seguro de borrar este mensaje?");
+
+                //Botón 'borrar'
+                builder.setPositiveButton("Borrar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        eliminarMensaje(i);
+                    }
+                });
+
+                //Botón 'cancelar'
+                builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        dialog.dismiss();
+                    }
+                });
+
+                //Crear y mostrar el cuadro de dialogo
+                builder.create().show();
+
+            }
+        });*/
+
         //settear el estado del mensaje
 
         if(i==chatList.size()-1){
@@ -95,6 +139,50 @@ public class AdaptadorChat extends RecyclerView.Adapter<AdaptadorChat.MyHolder> 
         }
 
     }
+
+    /*private void eliminarMensaje(int posicion) {
+
+        final String idUsuario1 = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        //-->> Comprobar la fecha del mensaje en la bbdd y si el elegido está en la conversación de ese chat
+        //-->> Si es así, borramos el mensaje
+
+        String fechaHora = chatList.get(posicion).getHoraDia();
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Chats");
+        Query query = dbRef.orderByChild("horadia").equalTo(fechaHora);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds: snapshot.getChildren()){
+
+                    //-->> Solo eliminar si el id del usuario actual coincide con quien lo está intentando borrar
+                    if(ds.child("enviado").getValue().equals(idUsuario1)){
+
+                        //Eliminar el mensaje del chat:
+                        //ds.getRef().removeValue(); //--> accedemos a la bbdd y borramos
+
+                        HashMap<String, Object> hashMap = new HashMap<>();
+                        hashMap.put("mensaje", "Este mensaje fue eliminado...");
+                        ds.getRef().updateChildren(hashMap); //-->> updateamos la bbdd por el mensaje de que fue eliminado
+
+                        Toast.makeText(context, "Mensaje borrado", Toast.LENGTH_SHORT).show();
+
+                    }else{
+                        Toast.makeText(context, "Solo puedes eliminar tus mensajes", Toast.LENGTH_SHORT).show();
+                    }
+
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }*/
 
     @Override
     public int getItemCount() {
@@ -119,6 +207,7 @@ public class AdaptadorChat extends RecyclerView.Adapter<AdaptadorChat.MyHolder> 
         //Vistas
         ImageView perfilChatIv;
         TextView mensajeTv, fechaHoraTv, vistoTv;
+        LinearLayout mensajeLayout; //-->> para el on click de ver el mensaje borrado
 
         public MyHolder(@NonNull View itemView) {
             super(itemView);
@@ -128,7 +217,7 @@ public class AdaptadorChat extends RecyclerView.Adapter<AdaptadorChat.MyHolder> 
             perfilChatIv = itemView.findViewById(R.id.perfilChatIv);
             mensajeTv = itemView.findViewById(R.id.mensajeTv);
             fechaHoraTv = itemView.findViewById(R.id.fechaHoraTv);
-            vistoTv = itemView.findViewById(R.id.vistoTv);
+            mensajeLayout = itemView.findViewById(R.id.mensajeLayout);
 
         }
     }
